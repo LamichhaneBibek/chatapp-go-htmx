e01343ef7454a391b4e407b8e393187fc36e4607
LamichhaneBibek <lamichhane.kebib@gmail.com>
2023-11-22 09:21:10 +0545

    added server.go file

---
+++ app/server.go
@@ -0,0 +1,48 @@
+// ================================================================================
+// Application entry point and HTTP server setup & start
+// ================================================================================
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	cookieKey := os.Getenv("COOKIE_KEY")
+	if cookieKey == "" {
+		cookieKey = "cookie-secret-1234567890"
+	}
+
+	echo := echo.New()
+
+	// Configure with HTML template renderer and session middleware
+	echo.HideBanner = true
+	htmlRenderer := NewHtmlRenderer("templates")
+	echo.Renderer = htmlRenderer
+	echo.Use(session.Middleware(sessions.NewCookieStore([]byte(cookieKey))))
+
+	// Open and/or create the database
+	db := openDB()
+	defer db.Close()
+
+	// Initialise the chat broker and add routes
+	broker := initChat(db, *htmlRenderer)
+	addRoutes(echo, broker, db)
+
+	// Start the server
+	log.Println("🚀 Starting HTMX chat server on port: " + port)
+	log.Println("✅ Version: " + os.Getenv("VERSION"))
+
+	echo.Logger.Fatal(echo.Start(":" + port))
+}
